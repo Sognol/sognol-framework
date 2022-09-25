52a5faa32b4ed50cfa2ca241583ea9f04f8f9105
Jesús Ropero <sognol.developer@gmail.com>
2022-09-26 00:16:07 +0200

    creación de librerias con métodos http

---
+++ Router.php
@@ -0,0 +1,24 @@
+<?php
+
+require "./HttpMethod.php";
+
+class Router {
+    protected array $routes = [];
+
+    public function __construct()
+    {
+        foreach (HttpMethod::cases() as $method) {
+            $this->routes[$method->value] = [];
+        }
+    }
+
+    public function get(string $uri, callable $action)
+    {
+        $this->routes[HttpMethod::GET->value][$uri] = $action;
+    }
+
+    public function post(string $uri, callable $action)
+    {
+        $this->routes[HttpMethod::POST->value][$uri] = $action;
+    }
+}
